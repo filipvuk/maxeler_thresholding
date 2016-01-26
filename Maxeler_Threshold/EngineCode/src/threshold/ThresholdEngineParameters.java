6184e81f67aa6ed0391f23412d92a6cd3896e95e
filipvuk <filip.vukovickv@gmail.com>
2016-01-26 21:47:07 +0100

    initial commit

---
+++ Maxeler_Threshold/EngineCode/src/threshold/ThresholdEngineParameters.java
@@ -0,0 +1,9 @@
+package threshold;
+
+import com.maxeler.maxcompiler.v2.build.EngineParameters;
+
+public class ThresholdEngineParameters extends EngineParameters {
+	public ThresholdEngineParameters(String[] args) {
+		super(args);
+	}
+}
